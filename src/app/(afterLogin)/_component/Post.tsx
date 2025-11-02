49c2d8cfbba3f20838ef84d45b5258e0a355e3c2
youngjjje <1443009@a.ut.ac.kr>
2025-11-02 22:50:47 +0900

    second commit 2-2.1

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -1,53 +1,68 @@
-import style from './post.module.css'
-import Link from "next/link"
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
-import 'dayjs/locale/ko'
-import ActionButton from "@/app/(afterLogin)/_component/ActionButtons"
+import style from './post.module.css';
+import Link from "next/link";
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
+import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
+import PostArticle from "@/app/(afterLogin)/_component/PostArticle";
+import {faker} from '@faker-js/faker';
+import PostImages from "@/app/(afterLogin)/_component/PostImages";
 
-dayjs.locale('ko')
+dayjs.locale('ko');
 dayjs.extend(relativeTime)
 
-export default function Post() {
-    const targetr = {
-        User: {
-            id: 'elonmusk',
-            nickname: 'Elon Musk',
-            image: '/yRsRRjGO.jpg'
-        },
-        content: '클론코딩 라이브로 하니 너무 힘들어요 ㅜㅜ',
-        createdAt: new Date(),
-        Images: []
-    }
-
-    return (
-        <article className={style.post}>
-            <div className={style.postWrapper}>
-                <div className={style.postUserSection}>
-                    <Link href={`/${targetr.User.id}`} className={style.postUserImage}>
-                        <img src={targetr.User.image} alt={targetr.User.nickname}/>
-                        <div className={style.postShade}/>
-                    </Link>
-                </div>
-                <div className={style.postBody}>
-                    <div className={style.postMeta}>
-                        <Link href={`${targetr.User.id}`}>
-                            <span className={style.postUserName}>{targetr.User.nickname}</span>
-                            &nbsp
-                            <span className={style.postUserId}>@{targetr.User.id}</span>
-                            &nbsp
-                            .
-                            &nbsp
-                        </Link>
-                        <span className={style.postDate}>{dayjs(targetr.createdAt).fromNow(true)}</span>
-                    </div>
-                    <div>{targetr.content}</div>
-                    <div className={style.postImageSection}>
-
-                    </div>
-                    <ActionButton />
-                </div>
-            </div>
-        </article>
+type Props = {
+  noImage?: boolean
+}
+export default function Post({ noImage }: Props) {
+  const target = {
+    postId: 1,
+    User: {
+      id: 'elonmusk',
+      nickname: 'Elon Musk',
+      image: '/yRsRRjGO.jpg',
+    },
+    content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
+    createdAt: new Date(),
+    Images: [] as any[],
+  }
+  if (Math.random() > 0.5 && !noImage) {
+    target.Images.push(
+      {imageId: 1, link: faker.image.urlLoremFlickr()},
+      {imageId: 2, link: faker.image.urlLoremFlickr()},
+      {imageId: 3, link: faker.image.urlLoremFlickr()},
+      {imageId: 4, link: faker.image.urlLoremFlickr()},
     )
+  }
+
+  return (
+    <PostArticle post={target}>
+      <div className={style.postWrapper}>
+        <div className={style.postUserSection}>
+          <Link href={`/${target.User.id}`} className={style.postUserImage}>
+            <img src={target.User.image} alt={target.User.nickname}/>
+            <div className={style.postShade}/>
+          </Link>
+        </div>
+        <div className={style.postBody}>
+          <div className={style.postMeta}>
+            <Link href={`/${target.User.id}`}>
+              <span className={style.postUserName}>{target.User.nickname}</span>
+              &nbsp;
+              <span className={style.postUserId}>@{target.User.id}</span>
+              &nbsp;
+              ·
+              &nbsp;
+            </Link>
+            <span className={style.postDate}>{dayjs(target.createdAt).fromNow(true)}</span>
+          </div>
+          <div>{target.content}</div>
+          <div>
+            <PostImages post={target} />
+          </div>
+          <ActionButtons/>
+        </div>
+      </div>
+    </PostArticle>
+  )
 }
